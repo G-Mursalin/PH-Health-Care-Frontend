14e1be408cf14146232b0ef401d93880e216b4d1
Golam Mursalin <golammursalin309@gmail.com>
2024-05-09 19:58:38 +0600

    Get and create schedules functionality add in admin dashboard

---
+++ src/redux/api/scheduleApi.ts
@@ -0,0 +1,51 @@
+import { baseApi } from "./baseApi";
+import { tagTypes } from "../tag-types";
+import { IMeta } from "@/types/common";
+
+export const scheduleApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    // Create Schedule
+    createSchedule: build.mutation({
+      query: (data) => ({
+        url: "/schedule",
+        method: "POST",
+        data,
+      }),
+      invalidatesTags: [tagTypes.schedule],
+    }),
+
+    // Get All Schedule
+    getAllSchedules: build.query({
+      query: (arg: Record<string, any>) => {
+        return {
+          url: "/schedule",
+          method: "GET",
+          params: arg,
+        };
+      },
+
+      transformResponse: (response: [], meta: IMeta) => {
+        return {
+          schedules: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.schedule],
+    }),
+
+    // Delete Schedule
+    deleteSchedule: build.mutation({
+      query: (id) => ({
+        url: `/schedule/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [tagTypes.schedule],
+    }),
+  }),
+});
+
+export const {
+  useCreateScheduleMutation,
+  useGetAllSchedulesQuery,
+  useDeleteScheduleMutation,
+} = scheduleApi;
